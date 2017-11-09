76fc59730369419bd4a831e5063c12ca339c1b43
ctranjith <33344477+ctranjith@users.noreply.github.com>
2017-11-09 17:33:39 +0530

    Update MainApp.java

--- HelloMessage/src/com/tutorialspoint/MainApp.java
+++ HelloMessage/src/com/tutorialspoint/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp {
       obj.setMessage("First ");
       obj.setTestValue("Test Value");
       obj.getMessage();   
-      
+      //test comment
       HelloMessage obj2 = (HelloMessage) context.getBean("HelloMessage");
       obj2.getMessage();
       ((ClassPathXmlApplicationContext)context).close();
